5d0d8b0d63b3cedd529e13e0df47f62e834ef032
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-02 01:51:37 -0800

    project complete

--- app.js
+++ app.js
@@ -1,68 +1,77 @@
 import inquirer from "inquirer";
-console.log("hello");
-const answer = await inquirer.prompt([
-    // prompt user to get user ID
-    {
-        type: "input",
-        name: "userId",
-        message: "Kindly enter your Id:",
-    },
-    // prompt user to get user PIN
-    {
-        type: "number",
-        name: "userPin",
-        message: "Kindly enter your Pin:",
-    },
-    // prompt user to get user account type
-    {
-        type: "list",
-        name: "accountType",
-        choices: ["Current", "Saving"],
-        message: "Kindly Select your Account Type:",
-    },
-    // prompt user to get transaction type
-    {
-        type: "list",
-        name: "transactionType",
-        choices: ["Fast Cash", "Withdraw"],
-        message: "Kindly Select your Transaction:",
-        // it run when accountType will run
-        when(answer) {
-            return answer.accountType;
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+(async () => {
+    await showBanner("A T M", "ATM allows you to make transection effectivilly and efficientlly ", "red", "blue");
+})();
+async function fun() {
+    const answer = await inquirer.prompt([
+        // prompt user to get user ID
+        {
+            type: "input",
+            name: "userId",
+            message: gradient.rainbow("Kindly enter your Id:"),
         },
-    },
-    //prompt user to select their withdraw amout from "fast Cash" transection type
-    {
-        type: "list",
-        name: "amount",
-        choices: [1000, 2000, 5000, 10000, 15000, 20000],
-        message: "Kindly Select your Withdraw amount:",
-        // it run when transactionType is "fast cash"
-        when(answer) {
-            return answer.transactionType === "Fast Cash";
+        // prompt user to get user PIN
+        {
+            type: "number",
+            name: "userPin",
+            message: gradient.rainbow("Kindly enter your Pin:"),
         },
-    },
-    // prompt user to enter their withdraw amout from "withdraw" transection type
-    {
-        type: "number",
-        name: "amount",
-        message: "Kindly Enter your Withdraw amount:",
-        // it run when transactionType is "Withdraw"
-        when(answer) {
-            return answer.transactionType === "Withdraw";
+        // prompt user to get user account type
+        {
+            type: "list",
+            name: "accountType",
+            choices: ["Current", "Saving"],
+            message: gradient.rainbow("Kindly Select your Account Type:"),
         },
-    },
-]);
-if (answer.accountType && answer.transactionType) {
-    const balance = Math.floor(Math.random() * 100000);
-    console.log(`Your balance is :${balance}`);
-    const userWithdrewAmount = answer.amount;
-    if (balance > userWithdrewAmount) {
-        const remainingBalance = balance - userWithdrewAmount;
-        console.log(`Your withdraw amount is :${answer.amount}`);
-        console.log(`Your Remaining balance is :${remainingBalance}`);
-    }
-    else {
-        console.log("insuficient balannce");
+        // prompt user to get transaction type
+        {
+            type: "list",
+            name: "transactionType",
+            choices: ["Fast Cash", "Withdraw"],
+            message: gradient.rainbow("Kindly Select your Transaction:"),
+            // it run when accountType will run
+            when(answer) {
+                return answer.accountType;
+            },
+        },
+        //prompt user to select their withdraw amout from "fast Cash" transection type
+        {
+            type: "list",
+            name: "amount",
+            choices: [1000, 2000, 5000, 10000, 15000, 20000],
+            message: gradient.rainbow("Kindly Select your Withdraw amount:"),
+            // it run when transactionType is "fast cash"
+            when(answer) {
+                return answer.transactionType === "Fast Cash";
+            },
+        },
+        // prompt user to enter their withdraw amout from "withdraw" transection type
+        {
+            type: "number",
+            name: "amount",
+            message: gradient.rainbow("Kindly Enter your Withdraw amount:"),
+            // it run when transactionType is "Withdraw"
+            when(answer) {
+                return answer.transactionType === "Withdraw";
+            },
+        },
+    ]);
+    if (answer.accountType && answer.transactionType) {
+        const balance = Math.floor(Math.random() * 100000);
+        console.log(gradient.rainbow(`Your balance is :${balance}`));
+        const userWithdrewAmount = answer.amount;
+        if (balance > userWithdrewAmount) {
+            const remainingBalance = balance - userWithdrewAmount;
+            console.log(gradient.rainbow(`Your withdraw amount is :${answer.amount}`));
+            console.log(gradient.rainbow(`Your Remaining balance is :${remainingBalance}`));
+        }
+        else {
+            console.log(gradient.rainbow("insuficient balannce"));
+        }
     }
 }
+setTimeout(() => {
+    fun();
+}, 1000);
